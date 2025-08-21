a50af725d6b5896a65ae3d6067ae86209aa86330
ridhimagupta449 <ridhimag449@gmail.com>
2025-08-21 23:37:32 +0530

    Add files via upload

---
+++ DS assign-2 q-1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[100],n,i,target;
+    cout<<"Enter the no. of elements in your array:"<<endl;
+    cin>>n;
+    cout<<"Enter the elements:"<<endl;
+    for(i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"Enter the element you wish to search:";
+    cin>>target;
+    int low=0,high=n-1;
+    while(low <= high){
+        int mid;
+        mid = (low + high)/2;
+        if(arr[mid] == target){
+    cout<<"element found at"<< " " << mid + 1 <<" "<<"position"<<endl;
+        break;
+        }
+        else if(arr[mid] < target){
+            low=mid+1;
+        }
+        else if(arr[mid] > target){
+            high=mid-1;
+        }
+
+    }
+return 0;
+
+}
